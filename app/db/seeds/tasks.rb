2df3a0b20009ef452fbfb738bebcfab9edb7b6d5
Steve <steve.hook@gmail.com>
2014-10-25 15:45:41 +0100

    Update seeds file to include a sample user

--- app/db/seeds/tasks.rb
+++ app/db/seeds/tasks.rb
@@ -7,13 +7,19 @@ require 'sequel'
 DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
 
 DB.run "DELETE FROM tasks"
+DB.run "DELETE FROM users"
 
-DB.run "INSERT INTO tasks(title, completed, complete_by)
-        VALUES('Walk the dog', false, '2014-08-10')"
-DB.run "INSERT INTO tasks(title, completed, complete_by)
-        VALUES('Cook dinner', false, '2014-08-10')"
-DB.run "INSERT INTO tasks(title, completed, complete_by)
-        VALUES('Learn node.js', false, '2014-08-14')"
-DB.run "INSERT INTO tasks(title, completed, complete_by, archived_at)
-        VALUES('Learn Ruby on Rails', false, '2012-08-14', '2014-08-14')"
+DB.run "INSERT INTO users(name, email)
+        VALUES('Bob Roberts', 'bob@example.com')"
+
+user_id = DB[:users].first[:id]
+
+DB.run "INSERT INTO tasks(title, completed, complete_by, user_id)
+        VALUES('Walk the dog', false, '2014-08-10', #{user_id})"
+DB.run "INSERT INTO tasks(title, completed, complete_by, user_id)
+        VALUES('Cook dinner', false, '2014-08-10', #{user_id})"
+DB.run "INSERT INTO tasks(title, completed, complete_by, user_id)
+        VALUES('Learn node.js', false, '2014-08-14', #{user_id})"
+DB.run "INSERT INTO tasks(title, completed, complete_by, archived_at, user_id)
+        VALUES('Learn Ruby on Rails', false, '2012-08-14', '2014-08-14', #{user_id})"
 
