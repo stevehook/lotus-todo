8e16792494a60806ce0ce4bc6d76d016a5ec2d41
Steve <steve.hook@gmail.com>
2015-06-16 20:20:23 +0100

    Add first React component to act as top-level todo list

---
+++ react-app/js/app.js
@@ -0,0 +1,5 @@
+React = require('react');
+
+var TodoApp = require('./components/TodoApp.react');
+
+React.render(<TodoApp />, document.getElementById('todoapp'));
---
+++ react-app/js/components/TodoApp.react.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  render: function() {
+  	return (
+      <div>Todos go here</div>
+  	);
+  },
+
+});
+
+module.exports = TodoApp;
