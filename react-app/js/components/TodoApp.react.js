77660c779c9eb70f34e705147ea7aa6988d2f9e1
Steve Hook <steve.hook@gmail.com>
2015-06-17 14:02:57 +0100

    First attempt to render a list

--- react-app/js/components/TodoApp.react.js
+++ react-app/js/components/TodoApp.react.js
@@ -1,10 +1,24 @@
 var React = require('react');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return { tasks: [
+      { id: 123, title: 'Walk the dog', completed: false },
+      { id: 456, title: 'Cook dinner', completed: false },
+      { id: 789, title: 'Go to the pub', completed: true }
+    ] };
+  },
+
+  createTask: function(task) {
+    console.log(task.id);
+    return (
+      <li id='task-{task.id}'>{task.title}</li>
+    );
+  },
 
   render: function() {
   	return (
-      <div>Todos go here</div>
+      <div><ul>{this.state.tasks.map(this.createTask)}</ul></div>
   	);
   },
 
