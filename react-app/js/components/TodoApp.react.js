8e37e464027d3204bd302cacd2a39cfb09de1aa5
Steve Hook <steve.hook@gmail.com>
2015-09-19 17:00:50 +0100

    Add a new component for entering new tasks

--- react-app/js/components/TodoApp.react.js
+++ react-app/js/components/TodoApp.react.js
@@ -3,20 +3,27 @@ var TodoTask = require('./TodoTask.react');
 
 var TodoApp = React.createClass({
   getInitialState: function() {
-    return { tasks: [
-      { id: 123, title: 'Walk the dog', completed: false },
-      { id: 456, title: 'Cook dinner', completed: false },
-      { id: 789, title: 'Go to the pub', completed: true }
-    ] };
+    return {
+      tasks: [
+        { id: 123, title: 'Walk the dog', completed: false },
+        { id: 456, title: 'Cook dinner', completed: false },
+        { id: 789, title: 'Go to the pub', completed: true }
+      ],
+      newTask: { id: 0, title: '', completed: false }
+    };
   },
 
   render: function() {
   	return (
-      <div><ul>{this.state.tasks.map(function(task) {
-        return (
-          <TodoTask task={task}/>
-        );
-      })}</ul></div>
+      <div>
+        <TodoNewTask task={newTask}/>
+        <div><ul>{this.state.tasks.map(function(task) {
+          return (
+            <TodoTask task={task}/>
+          );
+        })}</ul>
+        </div>
+      </div>
   	);
   },
 
---
+++ react-app/js/components/TodoNewTask.react.js
@@ -0,0 +1,25 @@
+var React = require('react');
+
+var TodoTask = React.createClass({
+  render: function() {
+  	return (
+      <input type='text' value='{this.props.task.title}' onKeyDown={this.handleNewTodoKeyDown}></input>
+  	);
+  },
+
+  handleNewTodoKeyDown: function (event) {
+    if (event.keyCode !== ENTER_KEY) {
+      return;
+    }
+
+    event.preventDefault();
+
+    var val = React.findDOMNode(this.refs.newField).value.trim();
+
+    if (val) {
+      console.log('New task: ', val);
+    }
+  }
+});
+
+module.exports = TodoTask;
