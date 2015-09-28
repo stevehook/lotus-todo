0250acf8753bcb12a96754196a1733d85cbddef4
Steve Hook <steve.hook@gmail.com>
2015-09-28 20:41:49 +0100

    Raise an event when user clicks the complete button

--- react-app/js/components/TodoTask.react.js
+++ react-app/js/components/TodoTask.react.js
@@ -4,12 +4,16 @@ var TodoTask = React.createClass({
   render: function() {
   	return (
       <li key={'task-' + this.props.task.id} className={this.props.task.completed ? 'completed' : 'pending'}>
-        <span className='glyphicon glyphicon-ok'></span>
+        <span className='glyphicon glyphicon-ok' onClick={this.handleComplete}></span>
         <span>{this.props.task.title}</span>
         <span className='glyphicon glyphicon-remove'></span>
       </li>
   	);
   },
+
+  handleComplete: function() {
+    this.props.completeTask(this.props.task.id);
+  }
 });
 
 module.exports = TodoTask;
