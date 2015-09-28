f54a4918fbb6b7dc0c9b786c31470f961a8322e1
Steve Hook <steve.hook@gmail.com>
2015-09-28 20:30:02 +0100

    Add glyphicons for completing and deleting tasks

--- react-app/js/components/TodoTask.react.js
+++ react-app/js/components/TodoTask.react.js
@@ -3,7 +3,11 @@ var React = require('react');
 var TodoTask = React.createClass({
   render: function() {
   	return (
-      <li key={'task-' + this.props.task.id}>{this.props.task.title}</li>
+      <li key={'task-' + this.props.task.id}>
+        <span className='glyphicon glyphicon-ok'></span>
+        <span>{this.props.task.title}</span>
+        <span className='glyphicon glyphicon-remove'></span>
+      </li>
   	);
   },
 });
