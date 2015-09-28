554303abc4957587872a4e85f15dea03797c09d2
Steve Hook <steve.hook@gmail.com>
2015-09-28 20:37:00 +0100

    Add condition to switch class depending on task state

--- react-app/js/components/TodoTask.react.js
+++ react-app/js/components/TodoTask.react.js
@@ -3,7 +3,7 @@ var React = require('react');
 var TodoTask = React.createClass({
   render: function() {
   	return (
-      <li key={'task-' + this.props.task.id}>
+      <li key={'task-' + this.props.task.id} className={this.props.task.completed ? 'completed' : 'pending'}>
         <span className='glyphicon glyphicon-ok'></span>
         <span>{this.props.task.title}</span>
         <span className='glyphicon glyphicon-remove'></span>
