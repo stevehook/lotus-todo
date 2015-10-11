e98cdb41049bb7505e41f55403005ec72b7cffa6
Steve Hook <steve.hook@gmail.com>
2015-10-11 09:32:42 +0100

    Refactor spec

--- react-app/__tests__/TodoNewTask.react-test.js
+++ react-app/__tests__/TodoNewTask.react-test.js
@@ -5,7 +5,7 @@ var React = require('react/addons');
 var TodoNewTask = require('../js/components/TodoNewTask.react.js');
 var TestUtils = React.addons.TestUtils;
 
-describe('TodoList.react', () => {
+describe('TodoNewTask.react', () => {
   var newTask;
   var handleNewTaskInput = function(task) {
     newTask = task;
@@ -14,15 +14,14 @@ describe('TodoList.react', () => {
   var todoNewTask = TestUtils.renderIntoDocument(
     <TodoNewTask onNewTaskInput={handleNewTaskInput}/>
   );
+  var input = TestUtils.findRenderedDOMComponentWithTag(todoNewTask, 'input');
 
   it('renders an input element', () => {
-    var input = TestUtils.findRenderedDOMComponentWithTag(todoNewTask, 'input');
     expect(input).toBeDefined();
   });
 
   it('calls onNewTaskInput handler when user presses enter key', () => {
-    var input = TestUtils.findRenderedDOMComponentWithTag(todoNewTask, 'input').getDOMNode();
-    input.value = 'Walk the dog';
+    input.getDOMNode().value = 'Walk the dog';
     TestUtils.Simulate.keyDown(input, { keyCode: 13 });
     expect(newTask).toBeDefined();
     expect(newTask).toEqual('Walk the dog');
