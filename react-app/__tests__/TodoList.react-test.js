0701baf22a297654a6de28cadabad80c2bc40239
Steve Hook <steve.hook@gmail.com>
2015-10-19 22:17:14 +0100

    Stub the TaskService API in spec

--- react-app/__tests__/TodoList.react-test.js
+++ react-app/__tests__/TodoList.react-test.js
@@ -5,15 +5,26 @@ var React = require('react/addons');
 var TodoList = require('../js/components/TodoList.react.js');
 var TodoTask = require('../js/components/TodoTask.react.js');
 var TodoNewTask = require('../js/components/TodoNewTask.react.js');
+var TaskService = require('../js/services/TaskService');
 var TestUtils = React.addons.TestUtils;
 
 describe('TodoList.react', () => {
+  var handleDone;
+  var promise = { done: (callback) => { handleDone = callback; return promise; }, fail: () => { return promise; } };
+  TaskService.prototype.getOutstanding.mockReturnValue(promise);
+
   var todoList = TestUtils.renderIntoDocument(
     <TodoList/>
   );
 
   it('renders a TodoTask component for each task', () => {
     var tasks = TestUtils.scryRenderedComponentsWithType(todoList, TodoTask);
+    expect(tasks.length).toEqual(0);
+    handleDone([
+      { id: 123, title: 'Walk the dog' },
+      { id: 456, title: 'Clean the kitchen' },
+      { id: 789, title: 'Read a good book' },
+    ]);
     expect(tasks.length).toEqual(3);
   });
 
