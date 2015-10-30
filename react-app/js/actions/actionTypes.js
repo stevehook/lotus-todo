f06a2ecf5c21ae3bd1734b8335e9e41fd11e45fe
Steve Hook <steve.hook@gmail.com>
2015-10-30 20:04:52 +0000

    Add some action constants and action creators

---
+++ react-app/js/actions/actionTypes.js
@@ -0,0 +1,20 @@
+
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const ARCHIVE_TODO = 'ARCHIVE_TODO';
+
+
+export function addTodo(title) {
+  return { type: ADD_TODO, title };
+};
+
+export function completeTodo(index) {
+  return { type: COMPLETE_TODO, index };
+};
+
+export function archiveTodo(index) {
+  return { type: ARCHIVE_TODO, index };
+};
