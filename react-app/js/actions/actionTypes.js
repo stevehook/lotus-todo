0f31c2b26a8d7153d70910fd264a4d0c0eefee87
Steve Hook <steve.hook@gmail.com>
2015-11-01 16:47:04 +0000

    Enable object literal property shortcuts

--- react-app/js/actions/actionTypes.js
+++ react-app/js/actions/actionTypes.js
@@ -5,14 +5,14 @@ export const ADD_TODO = 'ADD_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
 export const ARCHIVE_TODO = 'ARCHIVE_TODO';
 
-// export function addTodo(title) {
-//   return { type: ADD_TODO, title };
-// };
+export function addTodo(title) {
+  return { type: ADD_TODO, title };
+};
 
-// export function completeTodo(index) {
-//   return { type: COMPLETE_TODO, index };
-// };
+export function completeTodo(index) {
+  return { type: COMPLETE_TODO, index };
+};
 
-// export function archiveTodo(index) {
-//   return { type: ARCHIVE_TODO, index };
-// };
+export function archiveTodo(index) {
+  return { type: ARCHIVE_TODO, index };
+};
