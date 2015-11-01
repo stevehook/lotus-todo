2917ff9e44deca76c60a980e663ed4a51d7c7959
Steve Hook <steve.hook@gmail.com>
2015-11-01 16:44:25 +0000

    Fix linting issues

--- react-app/js/actions/actionTypes.js
+++ react-app/js/actions/actionTypes.js
@@ -1,4 +1,3 @@
-
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 
@@ -6,15 +5,14 @@ export const ADD_TODO = 'ADD_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
 export const ARCHIVE_TODO = 'ARCHIVE_TODO';
 
+// export function addTodo(title) {
+//   return { type: ADD_TODO, title };
+// };
 
-export function addTodo(title) {
-  return { type: ADD_TODO, title };
-};
+// export function completeTodo(index) {
+//   return { type: COMPLETE_TODO, index };
+// };
 
-export function completeTodo(index) {
-  return { type: COMPLETE_TODO, index };
-};
-
-export function archiveTodo(index) {
-  return { type: ARCHIVE_TODO, index };
-};
+// export function archiveTodo(index) {
+//   return { type: ARCHIVE_TODO, index };
+// };
