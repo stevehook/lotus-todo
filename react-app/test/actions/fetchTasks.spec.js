d4b0591dd6442d26ba5d05121f2e84d8767fa899
Steve Hook <steve.hook@gmail.com>
2015-11-04 13:39:58 +0000

    Refactor and add outline spec for fetchTasks async action

---
+++ react-app/test/actions/fetchTasks.spec.js
@@ -0,0 +1,13 @@
+import expect from 'chai';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import * as actions from '../../js/actions/actionTypes';
+// import nock from 'nock';
+
+
+describe('actions', () => {
+  describe('fetchTasks', () => {
+    it('create the correct action', () => {
+    });
+  });
+});
