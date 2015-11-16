f4ae6d3ffb809545b5f2b7d52af841f3aa4d83ae
Steve Hook <steve.hook@gmail.com>
2015-11-16 18:20:43 +0000

    Remove mockStore from other async action creator specs

--- react-app/test/actions/fetchTasks.spec.js
+++ react-app/test/actions/fetchTasks.spec.js
@@ -1,48 +1,8 @@
 const expect = require('chai').expect;
 const request = require('superagent');
-import { applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import * as actions from '../../js/actions/actionTypes';
 import nock from 'nock';
-
-const middlewares = [thunk];
-
-function mockStore(getState, expectedActions, done) {
-  if (!Array.isArray(expectedActions)) {
-    throw new Error('expectedActions should be an array of expected actions.');
-  }
-  if (typeof done !== 'undefined' && typeof done !== 'function') {
-    throw new Error('done should either be undefined or function.');
-  }
-
-  function mockStoreWithoutMiddleware() {
-    return {
-      getState() {
-        return typeof getState === 'function' ?  getState() : getState;
-      },
-
-      dispatch(action) {
-        const expectedAction = expectedActions.shift();
-
-        try {
-          expect(action).to.eql(expectedAction);
-          if (done && !expectedActions.length) {
-            done();
-          }
-          return action;
-        } catch (e) {
-          done(e);
-        }
-      }
-    }
-  }
-
-  const mockStoreWithMiddleware = applyMiddleware(
-    ...middlewares
-  )(mockStoreWithoutMiddleware);
-
-  return mockStoreWithMiddleware();
-}
+import mockStore from '../mockStore';
 
 describe('actions', () => {
   describe('fetchTasks', () => {
